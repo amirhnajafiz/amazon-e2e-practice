5a9b40f124e79e121fc2fb3f57e5f4637f5f634d
amirhnajafiz <najafizadeh21@gmail.com>
2025-06-07 14:13:09 -0400

    init: db

---
+++ backend/internal/database/users.go
@@ -0,0 +1,33 @@
+package database
+
+import "github.com/amirhnajafiz/aep/backend/pkg/models"
+
+func (db Database) GetAllUsers() ([]*models.User, error) {
+	var users []*models.User
+
+	return users, nil
+}
+
+func (db Database) GetUserByID(id string) (*models.User, error) {
+	var user *models.User
+
+	return user, nil
+}
+
+func (db Database) CreateUser(user *models.User) (*models.User, error) {
+	// Here you would typically insert the user into the database
+	// and return the created user object, possibly with an ID assigned by the database.
+	return user, nil
+}
+
+func (db Database) UpdateUser(user *models.User) (*models.User, error) {
+	// Here you would typically update the user in the database
+	// and return the updated user object.
+	return user, nil
+}
+
+func (db Database) DeleteUser(id string) error {
+	// Here you would typically delete the user from the database
+	// based on the provided ID.
+	return nil
+}
