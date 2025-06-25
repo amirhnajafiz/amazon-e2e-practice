59cd4b2c0fdfb55a23a1b58974c2e9bc198f2b4e
amirhnajafiz <najafizadeh21@gmail.com>
2025-06-25 17:32:56 -0400

    add: url methods in database

---
+++ api/internal/database/urls.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"context"
+
+	"github.com/amirhnajafiz/aep/backend/internal/models"
+)
+
+// InsertUrl inserts a new URL into the database.
+func (db *Database) InsertUrl(address string, description string) error {
+	ctx := context.Background()
+
+	_, err := db.conn.NewInsert().Model(&models.Url{
+		Address:     address,
+		Description: description,
+	}).Exec(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetUrls retrieves all URLs from the database.
+func (db *Database) GetUrls() ([]*models.Url, error) {
+	ctx := context.Background()
+
+	var urls []*models.Url
+	err := db.conn.NewSelect().Model(&urls).Scan(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return urls, nil
+}
+
+// ClearUrls deletes all URLs from the database.
+func (db *Database) ClearUrls() error {
+	ctx := context.Background()
+
+	_, err := db.conn.NewDelete().Model(&models.Url{}).Exec(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- api/internal/models/urls.go
+++ api/internal/models/urls.go
@@ -3,7 +3,8 @@ package models
 // Url represents a URL entry in the database.
 type Url struct {
 	ID          int64  `bun:"id,pk,autoincrement"`
-	Address     string `bun:"address,unique"`
+	ShortenedID string `bun:"shortened_id,notnull"`
+	Address     string `bun:"address,notnull"`
 	Description string `bun:"description"`
 	CreatedAt   string `bun:"created_at,default:current_timestamp"`
 	UpdatedAt   string `bun:"updated_at,default:current_timestamp"`
