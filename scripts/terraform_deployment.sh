297d348055ced85e892cabfd73c81a41ed00bdd4
amirhnajafiz <najafizadeh21@gmail.com>
2025-07-07 13:36:23 -0400

    add: terraform scripts

---
+++ scripts/terraform_deployment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+terraform init
+terraform apply -auto-approve
+if [ $? -ne 0 ]; then
+    echo "Terraform deployment failed."
+    exit 1
+fi
+
+echo "Terraform deployment completed successfully."
---
+++ scripts/terraform_teardown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+terraform destroy -auto-approve
+if [ $? -ne 0 ]; then
+    echo "Terraform teardown failed."
+    exit 1
+fi
+
+echo "Terraform teardown completed successfully."
